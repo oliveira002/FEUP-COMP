be1940ad3c07ff95c03812b732af867ea8da57f3
oliveira <deadzin112@gmail.com>
2023-04-13 15:00:25 +0100

    test

--- src/main/pt/up/fe/comp2023/analysis/JmmSimpleAnalysis.java
+++ src/main/pt/up/fe/comp2023/analysis/JmmSimpleAnalysis.java
@@ -20,7 +20,7 @@ public class JmmSimpleAnalysis implements JmmAnalysis {
         stVisitor.visit(root,symbolTable);
 
         List<SemanticAnalysisVisitor> visitors = Arrays.asList(new ArrayAccess(),new OperationSemantics(),
-                new ThisSemantics(), new ConditionSemantics(), new MethodSemantics());
+                new ThisSemantics(), new ConditionSemantics());
 
         List<Report> reps = new ArrayList<>();
         for(SemanticAnalysisVisitor v: visitors) {
