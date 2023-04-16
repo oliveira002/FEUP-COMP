d4e9a0d9453be98f2a7f054956e6594c75d7d043
up202004187 <up202004187@up.pt>
2023-04-16 18:59:45 +0100

    fix bug where String[] in main not allowed

--- src/main/pt/up/fe/comp2023/ollir/Utils.java
+++ src/main/pt/up/fe/comp2023/ollir/Utils.java
@@ -2,12 +2,17 @@ package pt.up.fe.comp2023.ollir;
 
 public class Utils {
     public static String toOllirType(String type, boolean is_array){
-        if(is_array) return ".array.i32"; //only int[] is supported
+
+        StringBuilder ollirType = new StringBuilder();
+
+        if(is_array)
+            ollirType.append(".array");
         return switch (type){
-            case "int" -> ".i32";
-            case "boolean" -> ".bool";
-            case "String" -> ".String";
-            default -> "."+type;
+            case "int" -> ollirType.append(".i32").toString();
+            case "boolean" -> ollirType.append(".bool").toString();
+            case "String" -> ollirType.append(".String").toString();
+            case "void" -> ollirType.append(".V").toString();
+            default -> ollirType.append(".").append(type).toString();
         };
     }
 }
