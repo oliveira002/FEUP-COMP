aeea97dc4f6a7c69543220fdd128555c6288ff18
up202004187 <up202004187@up.pt>
2023-05-13 17:20:26 +0100

    fix

--- src/main/pt/up/fe/comp2023/ollir/JmmOptimizerVisitor.java
+++ src/main/pt/up/fe/comp2023/ollir/JmmOptimizerVisitor.java
@@ -610,18 +610,23 @@ public class JmmOptimizerVisitor extends AJmmVisitor<StringBuilder,List<String>>
     private List<String> methodCallVisit(JmmNode jmmNode, StringBuilder ollirCode){
         JmmNode calledNode = jmmNode.getJmmChild(0);
         String called;
+        StringBuilder called_code = new StringBuilder();
+        StringBuilder called_prefix = new StringBuilder();
         while (calledNode.getKind().equals(ASTDict.PARENTHESES)){
             calledNode = calledNode.getJmmChild(0);
         }
         if(calledNode.getKind().equals(ASTDict.THIS)){
             called = "this";
         }
+        else if(calledNode.getKind().equals(ASTDict.METHOD_CALL)){
+            List<String> called_aux = visit(calledNode, ollirCode);
+            called = called_aux.get(0);
+            called_prefix = new StringBuilder(called_aux.get(1));
+        }
         else{
             called = calledNode.get("var");
         }
 
-        StringBuilder called_code = new StringBuilder();
-        StringBuilder called_prefix = new StringBuilder();
 
         String method = jmmNode.get("var");
         String return_type = ".V";
@@ -636,7 +641,7 @@ public class JmmOptimizerVisitor extends AJmmVisitor<StringBuilder,List<String>>
         //Deal with called
         //Import, class -> invokestatic
         if(symbolTable.getParsedImports().contains(called) || symbolTable.getClassName().equals(called)){
-            called_code.append("\t".repeat(indent)).append("invokestatic(").append(called).append(",").append("\"").append(method).append("\"");
+            called_code.append(called_prefix).append("\t".repeat(indent)).append("invokestatic(").append(called).append(",").append("\"").append(method).append("\"");
 
             if(symbolTable.getClassName().equals(called)){
                 return_type = Utils.toOllirType(symbolTable.getReturnType(method).getName(), symbolTable.getReturnType(method).isArray());
@@ -644,15 +649,22 @@ public class JmmOptimizerVisitor extends AJmmVisitor<StringBuilder,List<String>>
         }
         //local var, method params, class field, this -> invokevirtual
         else{
-            String called_type = "."+symbolTable.getClassName();
-            Type called_type_aux = new Type("", false);
-            if(!calledNode.getKind().equals(ASTDict.THIS)){
+            String called_type;
+            Type called_type_aux = new Type("", false);;
+            if(calledNode.getKind().equals(ASTDict.THIS)){
+                called_type = "." + symbolTable.getClassName();
+            }
+            else if(calledNode.getKind().equals(ASTDict.METHOD_CALL)){
+                called_type_aux = symbolTable.getReturnType(calledNode.get("var"));
+                called_type = "";
+            }
+            else{
                 called_type_aux = symbolTable.getAnyType(called, this.method);
                 called_type = Utils.toOllirType(called_type_aux.getName(),called_type_aux.isArray());
             }
 
 
-            called_code.append("\t".repeat(indent)).append("invokevirtual(").append(called).append(called_type).append(",").append("\"").append(method).append("\"");
+            called_code.append("\t".repeat(indent)).append(called_prefix).append("invokevirtual(").append(called).append(called_type).append(",").append("\"").append(method).append("\"");
 
             if(!symbolTable.getParsedImports().contains(called_type_aux.getName()))
                 return_type = Utils.toOllirType(symbolTable.getReturnType(method).getName(), symbolTable.getReturnType(method).isArray());
@@ -705,7 +717,7 @@ public class JmmOptimizerVisitor extends AJmmVisitor<StringBuilder,List<String>>
             return List.of(" " + called_code.substring(indent),params_prefix.toString());
         }
         //This is really stupid, but I'm not changing it now
-        else if(parent.getKind().equals(ASTDict.BINARY_OP) || parent.getKind().equals(ASTDict.METHOD_CALL) || parent.getKind().equals(ASTDict.ARRAY_INDEX) || parent.getKind().equals(ASTDict.NOT_OP) || parent.getKind().equals(ASTDict.COMPARE_OP) || parent.getKind().equals(ASTDict.LOGICAL_OP)) {
+        else if(parent.getKind().equals(ASTDict.BINARY_OP) || parent.getKind().equals(ASTDict.PARENTHESES) || parent.getKind().equals(ASTDict.METHOD_CALL) || parent.getKind().equals(ASTDict.ARRAY_INDEX) || parent.getKind().equals(ASTDict.NOT_OP) || parent.getKind().equals(ASTDict.COMPARE_OP) || parent.getKind().equals(ASTDict.LOGICAL_OP)) {
             String temp = Utils.nextTemp();
             params_code.append(")").append(return_type).append(";\n\n");
             called_code.append(params_code);
